fea67d366c0c3b016e4561327bb7ec9deba1e556
João Jerónimo <joao.jeronimo.pro@gmail.com>
2024-09-13 23:46:22 +0000

    Código exemplo.

---
+++ Refusbisher/Src/refurb/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Refurbisher',
+    'version': 'ODOO_RELEASE.0.1',
+    'author': 'João Jerónimo',
+    'data': [
+        'views_and_actions.xml',
+    ],
+    'depends': [
+        'base','
+        ],
+    'auto_install': False,
+}
