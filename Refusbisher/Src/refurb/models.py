2e519937d0de9c9099a6d6019fa0bb5542058eb9
João Jerónimo <joao.jeronimo.pro@gmail.com>
2024-09-14 01:26:45 +0000

    Permitir fotos.

--- Refusbisher/Src/refurb/models.py
+++ Refusbisher/Src/refurb/models.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models, tools, _
 
+class IrAttachment(models.Model):
+    _inherit = 'ir.attachment'
+    photo_of_equipment_id = fields.Many2one("refurb.equipment", string="Equipment photo")
+
 class Equipment(models.Model):
     _name = 'refurb.equipment'
     
-    name = fields.Char("Name", "required=True)
+    name = fields.Char("Name", required=True)
     details = fields.Html("Details")
+    
+    photos_ids = fields.One2many("ir.attachment", inverse_name="photo_of_equipment_id", string="Equipment photos")
 
